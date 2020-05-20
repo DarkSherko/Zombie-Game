183d52364495ffc3bd6515460a5beef10ef39493
DarkSherko <sherko.loran@gmail.com>
2020-05-20 10:39:21 +0200

    Concept Element Zovid Shero
    Sherko

---
+++ Zovidgameconcept/main.ts
@@ -0,0 +1,37 @@
+window.addEventListener("load", () => {
+
+let game = document.getElementsByTagName("game")[0]
+let element = document.createElement("player")
+game.appendChild(element)
+
+let player = {
+    x: 100,
+    y: 100,
+    color: "Red"
+}
+
+element.style.backgroundColor = player.color
+
+gameloop()
+
+function gameloop() {
+    player.x = player.x + 2
+    player.y += 1 
+
+    element.style.transform = `translate(${player.x}px, ${player.y}px)`
+
+    requestAnimationFrame(gameloop)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+})
