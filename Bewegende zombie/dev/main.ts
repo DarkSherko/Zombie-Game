27fe332a5cbf634823a882b7e442cdcf2cf80b7b
0957822 <57303998+0957822@users.noreply.github.com>
2020-05-25 12:00:33 +0200

    Add files via upload

---
+++ Bewegende zombie/dev/main.ts
@@ -0,0 +1,22 @@
+window.addEventListener("click",() =>{
+    let game = document.getElementsByTagName("game")[0]
+    let element = document.createElement("ball")
+    game.appendChild(element)
+
+    let ball = {
+        x: 100,
+        y:100,
+    }
+    
+
+    gameLoop()
+
+    function gameLoop() {
+        ball.x = ball.x + 2
+        ball.y += 1
+
+        element.style.transform = `translate(${ball.x}px, ${ball.y}px)`
+
+        requestAnimationFrame(gameLoop)
+    }
+})
