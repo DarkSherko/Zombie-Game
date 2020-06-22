5425592e0050e9339536a23eb6805c43769b0f72
mmerveon <45422341+mmerveon@users.noreply.github.com>
2020-06-22 18:02:09 +0200

    score
    plek veranderd, nu onderin scherm

--- Combined Game!/level 1/script.js
+++ Combined Game!/level 1/script.js
@@ -1,30 +1,30 @@
-var character = document.getElementById("character");
-var enemy = document.getElementById("enemy");
-let score = 0
-const scoreDisplay = document.getElementById('score');
+var character = document.getElementById("character");   
+var enemy = document.getElementById("enemy");          
+let score = 0                                                
+const scoreDisplay = document.getElementById('score');         
 
-function jump(){
+function jump(){  //function jump for the zombie
     if(character.classList != "animate"){
         character.classList.add("animate");
     }
-    setTimeout(function(){
+    setTimeout(function(){                                     
         character.classList.remove("animate");
     }, 500);
 }
 
-var checkDead = setInterval(function(){
-    var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+var checkDead = setInterval(function(){  //checks when the zombie is dead  
+    var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));    
     var enemyLeft = parseInt(window.getComputedStyle(enemy).getPropertyValue("left"));
-    if(enemyLeft<150 && enemyLeft>0 && characterTop >=470){
+    if(enemyLeft<150 && enemyLeft>0 && characterTop >=470){     //how fast the enemy goes and how high the character goes 
         enemy.style.animation = "none";
         enemy.style.display = "none";
-        setTimeout(function(){location.replace("fail.html");});
-    } else {
+        setTimeout(function(){location.replace("fail.html");}); //when the character the enemy damage you are going to the fail page for retry 
+    } else {                                                    //if the character don't thouche the enemy the score is going up
         score++
         scoreDisplay.innerHTML = score
 
-        if (score >= 2100){
-            setTimeout(function(){location.replace("pass.html");});
+        if (score >= 2100){                                     //if the score is higher or equals to 2100 seconds you win the level 
+            setTimeout(function(){location.replace("pass.html");}); //and you are going to the pass file
         }
     }
 });
